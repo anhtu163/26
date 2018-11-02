819870b239de864a90aa01f395ea20feb6bb35ab
Luong Ngoc Vu <32901054+luong-ngoc-vu@users.noreply.github.com>
2018-11-02 13:04:12 +0700

    Update EBook.php
    Thêm ebook và hiển thị thông tin ebook

--- EBook.php
+++ EBook.php
@@ -14,8 +14,7 @@
 		<div id="menu-top">
 		<div id="top-content">
 			<div id="logo">
-				<img src="/26/logo.jpg" width="100" height="100">
-				
+				<a href="index.html"><img src="/26/logo.jpg" width="100" height="100"></a>				
 			</div>
 			<pre><br><br><br><br></pre>
 			<div class="tab-menu">
@@ -23,16 +22,166 @@
 					<li><a href="/26">Trang Chủ</a></li>
 					<li><a href="/26/ThongTin.php">Thông Tin</a></li>
 					<li><a href="/26/Ebook.php">Ebook</a></li>
-					<li><a href="/26/QA.php">FQA</a></li>
+					<li><a href="/26/QA.php">Q/A</a></li>
 				</ul>
 			</div>
 			<div id="top-line"></div>
 		</div>
 		</div>
 		<pre><br></pre>
-		<div id="">
-			<br>
-			<iframe src="https://drive.google.com/file/d/1c_U7rB1QV3z6ntzwK-Z0lFa7h6cbYEFB/preview" width="700" height="800"></iframe>
+		<ul>
+				<li><pre>               
+			     </pre></li>
+				<li>
+					<fieldset class="group-search" background = >
+						<legend class="legend">Tìm Kiếm</legend>
+						<form id="form-search" name="formSearch" method="get" align="right-side" onSubmit="return(onFormSearch());">
+							<table cellpadding="0" cellspacing="0">
+								<tbody>
+									<tr>
+										<td align="right">Tên Sách</td>
+										<td width="20"></td>
+										<td align="left">
+											<input type="text" placeholder="Tên Sách" id="tensach" value>
+										</td>
+									</tr>
+									<tr>
+										<td align="right">Thể Loại</td>
+										<td width="20"></td>
+										<td align="left">
+											<select name="s_theloai_id" id="s_theloai_id" onChange="s_category_parent_change(this)">
+												<option value="0" selected="selected">-- Tất cả --</option>							<option value="1">Phát triển bản thân</option>										<option value="2">Khởi nghiệp</option>										
+												<option value="3">Nghiên cứu khoa học</option>										<option value="4">Toán thế giới</option>								
+											</select>		
+										</td>
+									</tr>									
+									<tr>
+										<td align="right">Năm Xuất Bản</td>
+										<td width="20"></td>
+										<td align="left">
+											<input type="text"  placeholder="Năm Xuất Bản" id="namxuatban" value>
+										</td>
+									</tr>
+									<td colspan="3" style="padding:0px;" align="right">
+									<input type="submit" name="search" value="TÌM SÁCH" class="button">
+									</td>
+								</tbody>
+							</table>
+						</form>
+					</fieldset>
+				</li>
+				<script src="KiemTraTimSach.js"></script>
+				<li class="header-line"></li>
+				<li>
+					<ul>
+					</ul>
+				</li>
+				<li class="header-line"></li>
+				<li>
+					<fieldset class="group-login">
+						<legend class="legend">Đăng nhập</legend>
+						<form id="form-login" name="formLogin" method="get" align="right-side">
+							<table cellpadding="0" cellspacing="0">
+								<tbody>
+									<tr>
+										<td align="right">Username</td>
+										<td width="20"></td>
+										<td align="left">
+											<input type="text" placeholder="Tên Đăng Nhập" id="user" value style="width: 218px">
+										</td>
+									</tr>
+									<tr>
+										<td align="right">Password</td>
+										<td width="20"></td>
+										<td align="left">
+											<input type="text"  placeholder="Mật khẩu" id="pass" value style="width: 218px">
+										</td>
+									</tr>
+									<tr>
+										<td align="right">        </td>
+										<td width="20"></td>
+										<td align="left">											
+											<button type ="submit" class="button">Đăng Nhập</button>				
+											<a type="button" href="/26/DangKy.php">Đăng Ký Tài Khoản</a>
+										</td>
+									</tr>					
+								</tbody>
+							</table>
+						</form>
+					</fieldset>
+				</li>				
+				<script src="KiemTraDangNhap.js"></script>	
+			</ul>			
+			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		 	<ul>
+				<div id = "menu-top">
+					<div id = "top-content" >
+						<div id="top-line"></div>
+					</div>
+				</div>
+			</ul>
+		<div id="content" >
+			<div id="content-left">
+				<div class="tab-menu">
+					<ul>	
+						<li class="active">
+							<a href="#" style="font-size: 20px">Ebook</a>						
+						</li>
+					</ul>
+				</div>
+				<div id="ebooks" class="box second" >
+					<ul id="tabvang_box_1" class="tabvang_box row-books" style="">
+						<li>
+							<ul class="ebook">
+							<li>								
+								<a ebook_id="100" href="ebook content/100-Dac-Nhan-Tam.php">
+									<img src="ebook image/Dac_Nhan_Tam.jpg" class="ebook_image">
+								</a>   
+						    </li>
+							<li class="ebook-title">
+								<a ebook_id="100" class="tooltip" href="ebook content/100-Dac-Nhan-Tam.php">Đắc nhân tâm</a>
+							</li>
+							</ul>							
+						</li>
+						<li>
+							<ul class="ebook">
+							<li>								
+								<a ebook_id="105" href="ebook content/105-Quang-Ganh-Lo-Di-Va-Vui-Song.php">
+									<img src="ebook image/Quang_Ganh_Lo_Di_Va_Vui_Song.jpg" class="ebook_image">
+								</a>
+						    </li>
+							<li class="ebook-title">
+								<a ebook_id="105" class="tooltip" href="ebook content/105-Quang-Ganh-Lo-Di-Va-Vui-Song.php">Quẳng gánh lo đi và vui sống</a>
+							</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="ebook">
+							<li>								
+								<a ebook_id="101" href="ebook content/101-7-Thoi-Quen.php">
+									<img src="ebook image/7_Thoi_Quen.jpg" class="ebook_image">
+								</a>   
+						    </li>
+							<li class="ebook-title">
+								<a ebook_id="101" class="tooltip" href="ebook content/101-7-Thoi-Quen.php">7 Thói quen hiệu quả</a>
+							</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="ebook">
+							<li>								
+								<a ebook_id="103" href="ebook content/103-Di-An-Mot-Minh.php">
+									<img src="ebook image/Di_An_Mot_Minh.jpg" class="ebook_image">
+								</a>   
+						    </li>
+							<li class="ebook-title">
+								<a ebook_id="103" class="tooltip" href="ebook content/103-Di-An-Mot-Minh.php">Đừng bao giờ đi ăn một mình</a>
+							</li>
+							</ul>
+						</li>												
+					</ul>
+				</div>
+			</div>
 		</div>
 		</div>
 	</body>
