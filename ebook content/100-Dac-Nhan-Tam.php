7771d524da6f66f5705da25a208dc2d070fa5249
Luong Ngoc Vu <32901054+luong-ngoc-vu@users.noreply.github.com>
2018-11-02 13:10:10 +0700

    Tạo folder "ebook content" và "ebook image"
    Tạo folder "ebook content" chứa thông tin sách và "ebook image" chứa ảnh

---
+++ ebook content/100-Dac-Nhan-Tam.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+	<header>
+		<title>Kho EBook</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css" />
+		</style>
+	</header>
+	<body>
+	<div id="wrapper">
+		<!--<script>
+			alert("Chào mừng bạn đến với trang web Quản Lý Thư Viện");
+		</script>-->
+		<div id="menu-top">
+		<div id="top-content">
+			<div id="logo">
+				<img src="/26/logo.jpg" width="100" height="100">
+				
+			</div>
+			<pre><br><br><br><br></pre>
+			<div class="tab-menu">
+				<ul id="main-menu">           
+					<li><a href="/26">Trang Chủ</a></li>
+					<li><a href="/26/ThongTin.php">Thông Tin</a></li>
+					<li><a href="/26/Ebook.php">Ebook</a></li>
+					<li><a href="/26/QA.php">Q/A</a></li>
+				</ul>
+			</div>
+			<div id="top-line"></div>
+		</div>
+		</div>
+		<pre><br></pre>
+		<div id="content">
+			<div id="content-left">
+			<div class="tab-menu">				
+				<ul>
+					<li class="active">
+						<a href="#">Thông tin chi tiết</a>
+					</li>
+				</ul>
+			</div>
+			<div id="ebooks" class="box second">
+				<div id=first-content>
+					<ul style="padding-top: 20px">
+						<li style=height: 300px>
+							<div class="imgbox">
+								<img src="ebook image/Dac_Nhan_Tam.jpg" class="ebook_image">
+							</div>	
+							<ul class="extent">
+								<li>Số trang: <strong>386</strong></li>
+								<li>Lượt xem: <strong>1307</strong></li>
+								<li>Lượt tải: <strong>16</strong></li>
+								<li>Định dạng: <strong>pdf</strong></li>
+							</ul>
+						</li>
+						<li style="width:auto; float:none;">
+							<h2 class="title">Đắc nhân tâm</h2>
+							<div class="bookdes">								
+								<p style>
+									<strong>Thể loại:</strong>
+									<a>Phát triển bản thân</a>
+								</p>
+								<div>
+									<p>
+										<span style="font-family: arial, helvetica, sans-serif; font-size: small;"><strong>Đắc nhân tâm</strong> (được lòng người), tên tiếng Anh là <strong>How to Win Friends and Influence People</strong> là một quyển sách nhằm tự giúp bản thân (self-help) bán chạy nhất từ trước đến nay. Quyển sách này do Dale Carnegie viết và đã được xuất bản lần đầu vào năm 1936, nó đã được bán 15 triệu bản trên khắp thế giới. Nó cũng là quyển sách bán chạy nhất của New York Times trong 10 năm. Tác phẩm được đánh giá là cuốn sách đầu tiên và hay nhất trong thể loại này, có ảnh hưởng thay đổi cuộc đời đối với hàng triệu người trên thế giới.</span>
+									</p>
+									<p>
+										<span style="font-family: arial, helvetica, sans-serif; font-size: small;">Những vấn đề được chỉ ra trong đó đều là căn bản ứng xử giữa người với người. Nếu diễn giải theo từ ngữ bây giờ, có thể gọi đây là “giáo trình” giúp hiểu mình – hiểu người để thành công trong giao tiếp. Có ai sống mà không cần giao tiếp? Có bao nhiêu người ngày ngày mệt mỏi, khổ sở vì gặp phải các vấn đề trong giao tiếp? Vì thế, Đắc Nhân Tâm chính là cuốn sách dành cho mọi người. Con cái nên đọc – cha mẹ càng nên đọc, nhân viên nên đọc – sếp càng nên đọc, người quen nhau nên đọc – người lạ nhau càng nên đọc…. Và đó mới chính thật là lý do Đắc Nhân Tâm luôn lọt vào Top sách bán chạy nhất thế giới, dù đã ra đời cách đây gần 80 năm.</span>
+									<p>
+										<span style="font-family: arial, helvetica, sans-serif; font-size: small;"><strong>Đắc nhân tâm</strong> (được lòng người), tên tiếng Anh là <strong>How to Win Friends and Influence People</strong> là một quyển sách nhằm tự giúp bản thân (self-help) bán chạy nhất từ trước đến nay. Quyển sách này do Dale Carnegie viết và đã được xuất bản lần đầu vào năm 1936, nó đã được bán 15 triệu bản trên khắp thế giới. Nó cũng là quyển sách bán chạy nhất của New York Times trong 10 năm. Tác phẩm được đánh giá là cuốn sách đầu tiên và hay nhất trong thể loại này, có ảnh hưởng thay đổi cuộc đời đối với hàng triệu người trên thế giới.</span>
+									</p>
+									<p>
+										<span style="font-family: arial, helvetica, sans-serif; font-size: small;">Những vấn đề được chỉ ra trong đó đều là căn bản ứng xử giữa người với người. Nếu diễn giải theo từ ngữ bây giờ, có thể gọi đây là “giáo trình” giúp hiểu mình – hiểu người để thành công trong giao tiếp. Có ai sống mà không cần giao tiếp? Có bao nhiêu người ngày ngày mệt mỏi, khổ sở vì gặp phải các vấn đề trong giao tiếp? Vì thế, Đắc Nhân Tâm chính là cuốn sách dành cho mọi người. Con cái nên đọc – cha mẹ càng nên đọc, nhân viên nên đọc – sếp càng nên đọc, người quen nhau nên đọc – người lạ nhau càng nên đọc…. Và đó mới chính thật là lý do Đắc Nhân Tâm luôn lọt vào Top sách bán chạy nhất thế giới, dù đã ra đời cách đây gần 80 năm.</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+					
+				</div>
+			</div>
+		</div>
+		</div>
+		</div>
+	</body>
+</html>
---
+++ ebook content/105-Quang-Ganh-Lo-Di-Va-Vui-Song.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+	<header>
+		<title>Kho EBook</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css" />
+		</style>
+	</header>
+	<body>
+	<div id="wrapper">
+		<!--<script>
+			alert("Chào mừng bạn đến với trang web Quản Lý Thư Viện");
+		</script>-->
+		<div id="menu-top">
+		<div id="top-content">
+			<div id="logo">
+				<img src="/26/logo.jpg" width="100" height="100">
+				
+			</div>
+			<pre><br><br><br><br></pre>
+			<div class="tab-menu">
+				<ul id="main-menu">           
+					<li><a href="/26">Trang Chủ</a></li>
+					<li><a href="/26/ThongTin.php">Thông Tin</a></li>
+					<li><a href="/26/Ebook.php">Ebook</a></li>
+					<li><a href="/26/QA.php">Q/A</a></li>
+				</ul>
+			</div>
+			<div id="top-line"></div>
+		</div>
+		</div>
+		<pre><br></pre>
+		<div id="content">
+			<div id="content-left">
+			<div class="tab-menu">				
+				<ul>
+					<li class="active">
+						<a href="#">Thông tin chi tiết</a>
+					</li>
+				</ul>
+			</div>
+			<div id="ebooks" class="box second">
+				<div id=first-content>
+					<ul style="padding-top: 20px">
+						<li style=height: 300px>
+							<div class="imgbox">
+								<img src="ebook image/Quang_Ganh_Lo_Di_Va_Vui_Song.jpg" class="ebook_image">
+							</div>	
+							<ul class="extent">
+								<li>Số trang: <strong>300</strong></li>
+								<li>Lượt xem: <strong>1307</strong></li>
+								<li>Lượt tải: <strong>30</strong></li>
+								<li>Định dạng: <strong>pdf</strong></li>
+							</ul>
+						</li>
+						<li style="width:auto; float:none;">
+							<h2 class="title">Quẳng gánh lo đi và vui sống</h2>
+							<div class="bookdes">
+								<p>
+								    <strong>Danh mục:</strong>
+								    <a>Phát triển bản thân</a>
+								</p>
+								<p style>
+									<strong>Thể loại:</strong>
+									<a>Phát triển bản thân</a>
+								</p>
+								<div>
+									<p>
+										<span style="font-family: arial, helvetica, sans-serif; font-size: small;"><strong>Quẳng Gánh Lo Đi Và Vui Sống</strong> là cuốn sách mà cái tên đã nói lên tất cả nội dung chuyển tải trên những trang giấy.
+ 									    Bất kỳ ai đang sống đều sẽ có những lo lắng thường trực về học hành, công việc, 
+										những hoá đơn, chuyện nhà cửa,... Cuộc sống không dễ dàng giải thoát bạn khỏi căng thẳng, ngược lại, nếu quá lo lắng, bạn có thể mắc bệnh trầm cảm. Quẳng Gánh Lo Đi Và Vui Sống khuyên bạn hãy khóa chặt dĩ vãng và tương lai lại để sống trong cái phòng kín mít của ngày hôm nay. Mọi vấn đề đều có thể được giải quyết, chỉ cần bạn bình tĩnh và xác định đúng hành động cần làm vào đúng thời điểm.
+
+</span>
+									</p>
+									<p>
+										<span style="font-family: arial, helvetica, sans-serif; font-size: small;">Nói thì có vẻ dễ nhưng những vấn đề liên quan đến các trạng thái tinh thần chẳng bao giờ dễ giải quyết. Chấm dứt lo lắng là điều không thể nhưng bớt đi sự lo lắng thì có thể, chỉ cần bạn đủ quyết tâm.
+										Quẳng Gánh Lo Đi Và Vui Sống khuyên bạn những cách để giảm thiểu lo lắng 
+										rất đơn giản như chia sẻ nó với người khác, tìm cách giải quyết vấn đề, quên tất cả những điều lo lắng nằm ngoài tầm tay,... Cố gắng thực tập điều này hàng ngày và trong cuộc sống chắc hẳn bạn sẽ thành công, có thể, không được như bạn muốn, nhưng chỉ cần bớt đi một chút phiền muộn thì cuộc sống của bạn đã có thêm một niềm vui.</span>
+									<p>
+										<span style="font-family: arial, helvetica, sans-serif; font-size: small;"><strong>Quẳng Gánh Lo Đi Và Vui Sống</strong> khuyên bạn những cách để giảm thiểu lo lắng rất đơn giản như chia sẻ nó với người khác, tìm cách giải quyết vấn đề, quên tất cả những điều lo lắng nằm ngoài tầm tay,… Cố gắng thực tập điều này hàng ngày và trong cuộc sống chắc hẳn bạn sẽ thành công, có thể, không được như bạn muốn, nhưng chỉ cần bớt đi một chút phiền muộn thì cuộc sống của bạn đã có thêm một niềm vui.</span>
+									</p>
+									<p>
+										<span style="font-family: arial, helvetica, sans-serif; font-size: small;">“Quẳng gánh lo đi và vui sống” – Một cuốn sách đáng đọc để tâm hồn được thoải mái hơn, đặc biệt là những khi bạn cảm thấy bế tắc, tuyệt vong, gặp những khó khăn trong cuộc sống. Những bài học trong cuốn sách này rất hay và dễ áp dụng. Nhiều điều khi ta đọc vào sẽ nhận ra nó rất đỗi bình thường nhưng có lẽ chúng ta đã vô tình đi ngang qua mà không để ý đến. Hãy đọc và cảm nhận, nếu chưa hiểu thì đừng ngần ngại đọc lại.
+										</span>
+									</p>
+									<a href="https://drive.google.com/file/d/1ja-yEBTG8pRsFILtVnF-K-_tzrTDO5qq/view?usp=sharing" target="_blank">ĐỌC SÁCH ONLINE</a>							
+								</div>
+							</div>
+						</li>
+					</ul>
+					
+				</div>
+			</div>
+		</div>
+		</div>
+		</div>
+	</body>
+</html>
