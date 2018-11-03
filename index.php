03c151edaf36ca2982b189e4e58440541c60669d
Anh Từ <44427982+anhtu163@users.noreply.github.com>
2018-11-03 17:53:10 +0700

    Add files via upload
    sửa form tìm kiếm của trang chủ, sửa file css

--- index.php
+++ index.php
@@ -1,40 +1,41 @@
 <!DOCTYPE html>
 <html>
 	<header>
-		<title>Trang Chủ My Library</title>
+		<title>Kho EBook</title>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="style.css" />
+		</style>
 	</header>
 	<body>
-		<div id="wrapper">
+	<div id="wrapper">
 		<!--<script>
 			alert("Chào mừng bạn đến với trang web Quản Lý Thư Viện");
 		</script>-->
-			<div id="menu-top">
-				<div id="top-content">
-					<div id="logo">
-						<img src="/26/logo.jpg" width="100" height="100">
-					</div>
-					<pre><br><br><br><br></pre>
-					<div class="tab-menu">
-						<ul id="main-menu">           
-							<li><a href="/26">Trang Chủ</a></li>
-							<li><a href="/26/ThongTin.php">Thông Tin</a></li>
-							<li><a href=/26/Ebook.php>Ebook</a></li>
-							<li><a href="/26/QA.php">FAQ</a></li>
-						</ul>
-					</div>
-					<div id="top-line"></div>
-				</div>
+		<div id="menu-top">
+		<div id="top-content">
+			<div id="logo">
+				<a href="index.html"><img src="/26/logo.jpg" width="100" height="100"></a>				
 			</div>
-			<pre><br></pre>
-			<ul>
+			<pre><br><br><br><br></pre>
+			<div class="tab-menu">
+				<ul id="main-menu">           
+					<li><a href="/26">Trang Chủ</a></li>
+					<li><a href="/26/ThongTin.php">Thông Tin</a></li>
+					<li><a href="/26/Ebook.php">Ebook</a></li>
+					<li><a href="/26/QA.php">Q/A</a></li>
+				</ul>
+			</div>
+			<div id="top-line"></div>
+		</div>
+		</div>
+		<pre><br></pre>
+		<ul>
 				<li><pre>               
 			     </pre></li>
 				<li>
 					<fieldset class="group-search" background = >
 						<legend class="legend">Tìm Kiếm</legend>
-						<form id="form-search" name="formSearch" method="get" align="right-side">
+						<form id="form-search" name="formSearch" method="get" align="right-side" onSubmit="return(onFormSearch());">
 							<table cellpadding="0" cellspacing="0">
 								<tbody>
 									<tr>
@@ -48,16 +49,12 @@
 										<td align="right">Thể Loại</td>
 										<td width="20"></td>
 										<td align="left">
-											<input type="text"  placeholder="Thể Loại" id="theloai" value>
+											<select name="s_theloai_id" id="s_theloai_id" onChange="s_category_parent_change(this)">
+												<option value="0" selected="selected">-- Tất cả --</option>							<option value="1">Phát triển bản thân</option>										<option value="2">Khởi nghiệp</option>										
+												<option value="3">Nghiên cứu khoa học</option>										<option value="4">Toán thế giới</option>								
+											</select>		
 										</td>
-									</tr>
-									<tr>
-										<td align="right">Tác giả</td>
-										<td width="20"></td>
-										<td align="left">
-											<input type="text"  placeholder="Tác Giả" id="tacgia" value>
-										</td>
-									</tr>
+									</tr>									
 									<tr>
 										<td align="right">Năm Xuất Bản</td>
 										<td width="20"></td>
@@ -65,68 +62,64 @@
 											<input type="text"  placeholder="Năm Xuất Bản" id="namxuatban" value>
 										</td>
 									</tr>
-									<tr>
-										<td align="right">        </td>
-										<td width="20"></td>
-										<td align="left">
-											<button type ="submit">Tìm Kiếm</button>									
-										</td>
-									</tr>
+									<td colspan="3" style="padding:0px;" align="right">
+									<input type="submit" name="search" value="TÌM SÁCH" class="button">
+									</td>
 								</tbody>
 							</table>
 						</form>
 					</fieldset>
 				</li>
+				<script src="KiemTraTimSach.js"></script>
+				<li class="header-line"></li>
 				<li>
-					<pre><ul>
-
-					</ul></pre>
+					<ul>
+					</ul>
 				</li>
+				<li class="header-line"></li>
 				<li>
 					<fieldset class="group-login">
 						<legend class="legend">Đăng nhập</legend>
-						<form action="SignIn.php" id="form-login" name="formLogin" method="post" align="right-side" onSubmit="return(onFormSignIn());" >
+						<form id="form-login" name="formLogin" method="get" align="right-side">
 							<table cellpadding="0" cellspacing="0">
 								<tbody>
 									<tr>
 										<td align="right">Username</td>
 										<td width="20"></td>
 										<td align="left">
-											<input type="text" placeholder="Tên Đăng Nhập" id="User" name="User" style="width: 218px">
+											<input type="text" placeholder="Tên Đăng Nhập" id="user" value style="width: 218px">
 										</td>
 									</tr>
 									<tr>
 										<td align="right">Password</td>
 										<td width="20"></td>
 										<td align="left">
-											<input type="password" placeholder="Mật khẩu" id="Pass" name="Pass" style="width: 218px">
+											<input type="text"  placeholder="Mật khẩu" id="pass" value style="width: 218px">
 										</td>
 									</tr>
 									<tr>
 										<td align="right">        </td>
 										<td width="20"></td>
-										<td align="left">
-											<button type ="submit" class="button" name="dangnhap">Đăng Nhập</button>				
-											<a type="button" href="/26/DangKy.php"><u>Đăng Ký Tài Khoản</u></a>
+										<td align="left">											
+											<button type ="submit" class="button">Đăng Nhập</button>				
+											<a type="button" href="/26/DangKy.php">Đăng Ký Tài Khoản</a>
 										</td>
-									</tr>
+									</tr>					
 								</tbody>
 							</table>
 						</form>
 					</fieldset>
-					<script src="KiemTraDangNhap.js"></script>
-				</li>
-			</ul>
-			<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-			<ul>
+				</li>				
+				<script src="KiemTraDangNhap.js"></script>	
+			</ul>			
+			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		 	<ul>
 				<div id = "menu-top">
 					<div id = "top-content" >
 						<div id="top-line"></div>
 					</div>
 				</div>
-			</ul>	
+			</ul>
 		</div>
-
-		<script src="function.js"></script>
 	</body>
 </html>
