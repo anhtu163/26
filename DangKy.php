7d4f40e1034092f05d45cf6d658f358280281268
Luong Ngoc Vu <32901054+luong-ngoc-vu@users.noreply.github.com>
2018-11-05 20:23:03 +0700

    Update DangKy.php

--- DangKy.php
+++ DangKy.php
@@ -4,16 +4,14 @@
 		<title>Đăng ký tài khoản</title>
 		<meta charset="UTF-8">
 			<link rel="stylesheet" href="style.css" />
-	</header>
+	</header>		
+	
 	<body>
-		<div id="wrapper">
-			<!--<script>
-			alert("Chào mừng bạn đến với trang web Quản Lý Thư Viện");
-		</script>-->
+		<div id="wrapper">			
 			<div id="menu-top">
 				<div id="top-content">
 					<div id="logo">
-						<img src="/26/logo.jpg" width="100" height="100">
+						<a href="index.html"><img src="/26/logo.jpg" width="100" height="100"></a>
 					</div>
 					<pre><br><br><br><br></pre>
 					<div class="tab-menu">
@@ -21,70 +19,73 @@
 							<li><a href="/26">Trang Chủ</a></li>
 							<li><a href="/26/ThongTin.php">Thông Tin</a></li>
 							<li><a href=/26/Ebook.php>Ebook</a></li>
-							<li><a href="/26/QA.php">FAQ</a></li>
+							<li><a href="/26/QA.php">Q/A</a></li>
 						</ul>
 					</div>
 					<div id="top-line"></div>
 				</div>
-			</div>
-
-
+			</div>			
+			
 			<pre><br></pre>
 			<ul>
 				<li><pre>               
+				
 			     </pre></li>
+				
+				<li class="header-line"></li>
 				<li>
+					<ul>
+					</ul>
+				</li>
+				<li class="header-line"></li>
+				
+				<li>					
 					<fieldset class="group-signup">
-						<legend class="legend">Đăng Kí</legend>
-						<form id="form-signup" name="formSignup" method="get" align="right-side" onSubmit="return(onFormSignUp());">
+						<legend class="legend">Đăng kí</legend>
+						<form id="form-signup" name="formSignup" method="post" align="right-side" action="SignUp.php" 
+							  onSubmit="return(onFormSignUp());">		
 							<table cellpadding="0" cellspacing="0">
 								<tbody>
 									<tr>
 										<td align="right">Tên đăng nhập</td>
 										<td width="20"></td>
 										<td align="left">
-											<input type="text" placeholder="Tên đăng nhập" id="tendangnhap" value>
+											<input type="text" placeholder="Tên đăng nhập" id="tendangnhap" name="tendangnhap" value>
 										</td>
 									</tr>
 									<tr>
 										<td align="right">Email</td>
 										<td width="20"></td>
 										<td align="left">
-											<input type="email"  placeholder="Email" id="email" value style="width: 198px" ">
+											<input type="email"  placeholder="Email" id="email" value name="email" style="width: 198px" >
 										</td>
 									</tr>
 									<tr>
 										<td align="right">Mật khẩu</td>
 										<td width="20"></td>
 										<td align="left">
-											<input type="password"  placeholder="Mật khẩu" id="matkhau" value>
+											<input type="password"  placeholder="Mật khẩu" name="matkhau" id="matkhau" value>
 										</td>
 									</tr>
 									<tr>
 										<td align="right">Nhập lại mật khẩu</td>
 										<td width="20"></td>
 										<td align="left">
-											<input type="password"  placeholder="Nhập lại mật khẩu" id="nhaplaimatkhau" value >
+											<input type="password"  placeholder="Nhập lại mật khẩu" name="nhaplaimatkhau" id="nhaplaimatkhau" value >
 										</td>
 									</tr>
-									<tr>
-										<td align="right">        </td>
-										<td width="20"></td>
-										<td align="left">
-											<button type ="submit">Đăng kí</button>									
-									<!--<a type="button" href="/26/DangKy.php">Đăng Ký Tài Khoản</a>-->
-										</td>
-									</tr>
+									<td colspan="3" style="padding:0px;" align="right">
+									<input type="submit" name="signup" value="ĐĂNG KÍ" class="button">
+									</td>
 								</tbody>
 							</table>
 						</form>
 					</fieldset>
-				</li>
+					<script src="KiemTraDangKi.js"></script>
+				</li>				
+				
+				
 			</ul>	
-
-
-
-		</div>
-		<script src="KiemTraDangKi.js"></script>											  
+		</div>				
 	</body>
 </html>
